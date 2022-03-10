dca055196baf42945b0b8aeeac924fc3254b2219
snarne2009 <96262960+snarne2009@users.noreply.github.com>
2022-03-10 20:12:38 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+var canvas, angle, tower, ground, cannon;
+var ball = []
+var cannonball
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 100, 50, angle);
+ 
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  
+
+  Engine.update(engine);
+  ground.display();
+  
+for (var i=0;i < ball.length;i++){
+  showcannonball(ball[i],i)
+}
+  cannon.display();
+  tower.display();
+ 
+ 
+}
+
+function Keypressed(){
+  if (keyCode==DOWN_ARROW){
+   var  cannonball = new Cannonball(cannon.x, cannon.y);
+    ball.push(cannonball)
+    
+    
+  }
+  function showcannonball(balls,index){
+    balls.display()
+  }
+}
+
+
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    ball[ball.length-1].shoot()ss
+  }
+}
